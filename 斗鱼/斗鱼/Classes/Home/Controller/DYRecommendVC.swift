89efc42646093b7fc8d9836eb6930693ed36ae40
pba <314269883@qq.com>
2017-01-18 18:59:09 +0800

    定制首页cell

--- 斗鱼/斗鱼/Classes/Home/Controller/DYRecommendVC.swift
+++ 斗鱼/斗鱼/Classes/Home/Controller/DYRecommendVC.swift
@@ -12,8 +12,10 @@ import UIKit
 private let kItemMargin : CGFloat = 10
 private let kItemW : CGFloat = (kScreenW - 3 * kItemMargin) * 0.5
 private let kItemH : CGFloat = kItemW * 3 / 4
+private let kPrettyItemH : CGFloat = kItemW * 4 / 3
 private let kHeaderViewH : CGFloat = 40
 private let DYRecommendItemID = "DYRecommendItemID"
+private let DYRecommendPrettyItemID = "DYRecommendPrettyItemID"
 private let DYRecommendHeaderID = "DYRecommendHeaderID"
 
 class DYRecommendVC: UIViewController {
@@ -32,8 +34,10 @@ class DYRecommendVC: UIViewController {
         collectionView.backgroundColor = UIColor.white
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         collectionView.dataSource = self
+        collectionView.delegate = self
         //注册item
         collectionView.register(UINib(nibName: "DYCollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: DYRecommendItemID)
+        collectionView.register(UINib(nibName: "DYRecommendPrettyCell", bundle: nil), forCellWithReuseIdentifier: DYRecommendPrettyItemID)
         //注册头部视图
         collectionView.register(UINib(nibName: "DYRecommendHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: DYRecommendHeaderID)
         
@@ -62,7 +66,7 @@ extension DYRecommendVC {
 }
 
 // MARK: - UICollectionViewDataSource
-extension DYRecommendVC : UICollectionViewDataSource {
+extension DYRecommendVC : UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         
@@ -77,25 +81,45 @@ extension DYRecommendVC : UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        //1.创建item
-        let item = collectionView.dequeueReusableCell(withReuseIdentifier: DYRecommendItemID, for: indexPath)
         
-        //2.设置item
-//        item.backgroundColor = UIColor.red
+        if indexPath.section == 1 { //颜值
+            //1.创建item
+            let item = collectionView.dequeueReusableCell(withReuseIdentifier: DYRecommendPrettyItemID, for: indexPath)
+            
+            //2.设置item
+            
+            //3.返回item
+            return item
+        }else{
+            //1.创建item
+            let item = collectionView.dequeueReusableCell(withReuseIdentifier: DYRecommendItemID, for: indexPath)
+            
+            //2.设置item
+            
+            //3.返回item
+            return item
+        }
         
-        //3.返回item
-        return item
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         //1.创建header
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: DYRecommendHeaderID, for: indexPath)
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DYRecommendHeaderID, for: indexPath)
         
         //2.设置header
         
         
         //3.返回header
         return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath.section == 1 { //颜值
+            return CGSize(width: kItemW, height: kPrettyItemH)
+        }else{
+            return CGSize(width: kItemW, height: kItemH)
+        }
     }
     
 
---
+++ 斗鱼/斗鱼/Classes/Home/View/DYRecommendPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  DYRecommendPrettyCell.swift
+//  斗鱼
+//
+//  Created by pba on 2017/1/18.
+//  Copyright © 2017年 wenjun. All rights reserved.
+//
+
+import UIKit
+
+class DYRecommendPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
